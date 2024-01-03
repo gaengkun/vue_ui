f7284a7ee8839050c9d9bc3a80487a7e0597c0e1
gaengkun <40553508+gaengkun@users.noreply.github.com>
2024-01-03 11:06:23 +0900

    element plus

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import AutoImport from 'unplugin-auto-import/vite';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+   plugins: [
+      vue(),
+      AutoImport({
+         resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+         resolvers: [ElementPlusResolver()],
+      }),
+   ],
+   optimizeDeps: {
+      include: ['element-plus'], // 프로젝트의 실제 의존성에 맞게 조정
+   },
+   resolve: {
+      alias: {
+         '@': `${__dirname}/src`,
+         '@components': `${__dirname}/src/components`,
+      },
+   },
+});
